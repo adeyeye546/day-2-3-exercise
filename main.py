ed06d94f5961908f2ea63b6e59f0048518098f69
Adeyeye Gbenga <adeyeye546@gmail.com>
2021-02-19 20:59:55 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age?")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+myAge = int(age)
+
+agetogo = 90
+newage = agetogo - myAge
+
+
+days = newage * 365 
+weeks = newage * 52 
+months = newage * 12
+
+print(f"You have {days} days, {weeks} weeks, and {months} months left.")
+
+
+
+
+
+
+
+
